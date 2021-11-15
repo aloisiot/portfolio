c85dcd7c90c93880509807481a8dbac4789bf979
aloisiot <aloisiot.c98@gmail.com>
2021-11-15 19:50:57 -0300

    pequenos ajustes de layout | ajustando o comportamento do menu drop-down

--- src/components/DropDownMenu/index.jsx
+++ src/components/DropDownMenu/index.jsx
@@ -10,11 +10,11 @@ const DropDownMenu = () => {
     <>
         <button onClick={()=> setIsVisible(!isVisible)} id="toggle-drop-down-menu" className="btn"> { isVisible ? <CloseBusson/> : <MenuButton/> } </button>
         <ul className={`dropdown-menu dropdown-menu-macos mx-0 border-0 shadow ${ isVisible ? " is-visible ": ""}`}>
-            <li><a className="dropdown-item" href="#about-me">Sobre mim</a></li>
-            <li><a className="dropdown-item" href="#skills">Competências</a></li>
-            <li><a className="dropdown-item" href="#portfolio">Portfólioe</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#contact">Contato</a></li>
+            <li onClick={()=> setIsVisible(false)} ><a className="dropdown-item" href="#about-me">Sobre mim</a></li>
+            <li onClick={()=> setIsVisible(false)} ><a className="dropdown-item" href="#skills">Competências</a></li>
+            <li onClick={()=> setIsVisible(false)} ><a className="dropdown-item" href="#portfolio">Portfólio</a></li>
+            <li onClick={()=> setIsVisible(false)} ><hr className="dropdown-divider"/></li>
+            <li onClick={()=> setIsVisible(false)} ><a className="dropdown-item" href="#contact">Contato</a></li>
         </ul>
     </>
     );
