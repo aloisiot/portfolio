0b77972cf9da92c86579f65f3aaa622456a1062f
aloisiot <aloisiot.c98@gmail.com>
2021-11-16 10:37:00 -0300

    alterando sombra dos cards e reorganizando arquivo variaveis.scss

--- src/components/Portfolio/ProjectCard/index.jsx
+++ src/components/Portfolio/ProjectCard/index.jsx
@@ -1,7 +1,7 @@
 import './style.scss'
 const ProjectCard = ({project}) => {
     return(
-        <div className="project-card">
+        <div className="project-card shadow">
             <div className="img-reference d-flex align-items-center justify-content-center">
                 <img src={project.img.src} alt={project.img.alt} />
             </div>
