eab5f2cbe75d8cbf163ceb68cb9b8bf54eff7d76
aloisiot <aloisiot.c98@gmail.com>
2021-11-16 21:11:24 -0300

    substituindo lorem ipsum por texto definitivo na seçao AboutMe

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
     return(
         <section id="about-me" className="d-flex align-items-center flex-column">
             <h2 className="title">Sobre Mim</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gal. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+            <p>Estudante de Análise e desenvolvimento de Sistemas pela Doctum e seguindo a trilha Certify Dech Developer pela Digital House.  Estudo programação não somente em busca de uma carreira promissora, mas porque sou   apaixonado por essa área e pelas suas infinitas possibilidades. Adoro trabalhar em equipe e aprender coisas novas! Nas horas vagas sou praticante de musculação e patinação.</p>
             <img className="ilustration" src={devIlustration} alt="Ilustração 'O desenvolvedor'" />
         </section>
     );
