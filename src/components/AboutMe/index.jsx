5bafd83617056931748eafaa4b5d63c761234d36
aloisiot <aloisiot.c98@gmail.com>
2021-11-16 21:18:12 -0300

    Corrigindo erros sintaticos e ortograficos no texto

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
     return(
         <section id="about-me" className="d-flex align-items-center flex-column">
             <h2 className="title">Sobre Mim</h2>
-            <p>Estudante de Análise e desenvolvimento de Sistemas pela Doctum e seguindo a trilha Certify Dech Developer pela Digital House.  Estudo programação não somente em busca de uma carreira promissora, mas porque sou   apaixonado por essa área e pelas suas infinitas possibilidades. Adoro trabalhar em equipe e aprender coisas novas! Nas horas vagas sou praticante de musculação e patinação.</p>
+            <p>Estudante de Análise e Desenvolvimento de Sistemas pela Doctum e seguindo a trilha Certified Tech Developer pela Digital House. Estudo programação não apenas em busca de uma carreira promissora, mas por que sou apaixonado por essa área e pelas suas infinitas possibilidades. Adoro trabalhar em equipe e aprender coisas novas! Nas horas vagas sou praticante de musculação e patinação.</p>
             <img className="ilustration" src={devIlustration} alt="Ilustração 'O desenvolvedor'" />
         </section>
     );
